1267367fb18a16e3004bf8c566d6092e2b51aea4
Mweresa Esther Magoma <esthermweresa27@gmail.com>
2025-05-07 15:16:13 +0300

    compete tests

--- lib/list_crud.py
+++ lib/list_crud.py
@@ -1,26 +1,38 @@
-def create_an_empty_list():
-    return None
+# creating an empty list
+def create_an_empty_list():    
+    return [] #empty list
 
 def create_a_list():
-    return None
+    return [1,2,3,4] #a list with 4 elements
 
+#adding an element at the end of a list 
 def add_element_to_end_of_list(l, element):
-    return None
+    l.append(element)
+    return l
 
+#adding an element at the start of a list
 def add_element_to_start_of_list(l, element):
-    return None
+    l.insert(0, element)
+    return l
 
+#removing an element at the end of a list
 def remove_element_from_end_of_list(l):
-    return None
+    l.pop()
+    return l
 
+#removing an element at the start of a list
 def remove_element_from_start_of_list(l):
-    return None
+    del l[0]
+    return l
 
+#retrieving first element from a list
 def retrieve_first_element_from_list(l):
-    return None
+    return l[0]
 
+#retrieving element at an index
 def retrieve_element_from_index(l, index):
-    return None
+    return l[index]
 
+#retrieving last element in a list
 def retrieve_last_element_from_list(l):
-    return None
+    return l[-1]
